56778ea6be8ebce91c900dce3e0e388423680f2a
Brian Wu <brianwu@aaeon.com.tw>
2022-06-15 15:40:33 +0800

    Create aaeon smbios api dll project.

---
+++ AaeonSmbiosApi/AaeonSmbiosApi.h
@@ -0,0 +1,13 @@
+// AaeonSmbiosApi.h - Contains declarations of smbios feature functions
+#pragma once
+
+#ifdef AAEONSMBIOSAPI_EXPORTS
+#define AAEONSMBIOS_API __declspec(dllexport)
+#else
+#define AAEONSMBIOS_API __declspec(dllimport)
+#endif
+
+extern "C"
+{
+
+}
