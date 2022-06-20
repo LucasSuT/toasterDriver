4286dbc01217576766ca1a3f5234f50dd6845f82
Brian Wu <brianwu@aaeon.com.tw>
2022-06-20 17:50:01 +0800

    Fix issue when dll and console built with /MT will runtime error.
    1. Change parameter type from string to const string&.

--- AaeonSmbiosApi/AaeonSmbiosApi.h
+++ AaeonSmbiosApi/AaeonSmbiosApi.h
@@ -72,5 +72,5 @@ extern "C"
 	AAEONSMBIOS_API void AaeonSmbiosInitial();
 	AAEONSMBIOS_API void AaeonSmbiosUninitial();
 
-	AAEONSMBIOS_API bool AaeonSmbiosGetMemInfo(SmbiosType smbios_table_number, string member_name, SmbiosMemberInfo* member_info);
+	AAEONSMBIOS_API bool AaeonSmbiosGetMemInfo(SmbiosType smbios_table_number, const string& member_name, SmbiosMemberInfo* member_info);
 }
