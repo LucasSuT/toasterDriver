46dbc3e06261628cac7f0cf0269be84860fdf458
BrianWu <brianwu@aaeon.com.tw>
2022-08-08 10:01:04 +0800

    Fix smbios_tables type 2,3 table not added issue. Add a examination when table number out of range in current smbios_table.

--- AaeonSmbiosApi/AaeonSmbiosApi.cpp
+++ AaeonSmbiosApi/AaeonSmbiosApi.cpp
@@ -21,6 +21,7 @@ AAEONSMBIOS_API bool AaeonSmbiosGetMemInfo(SmbiosType smbios_table_number, const
 	{
 		if ( !member_info ) throw std::invalid_argument("member_info pointer is null");
 		if ( member_name.empty() ) throw std::invalid_argument("member_name is empty");
+		if ( smbios_table_number >= smbios_member->smbios_tables.size() ) throw std::invalid_argument("Not support selected table number");
 
 		smbios_member_object = smbios_member->smbios_tables[smbios_table_number].at(member_name);
 		member_info->type = smbios_member_object.type_;
@@ -31,6 +32,7 @@ AAEONSMBIOS_API bool AaeonSmbiosGetMemInfo(SmbiosType smbios_table_number, const
 	}
 	catch (const std::exception& e)
 	{
+		printf("Failed when retrieve SMBios Table [%d] - [%s] information.\n", smbios_table_number, member_name.c_str());
 		printf("[%s]: %s\n", typeid(e).name(), e.what());
 		return false;
 	}
