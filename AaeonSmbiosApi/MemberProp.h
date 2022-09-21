4dd08fdf33fffeefff540660248a581bb5fa4bec
FredHung <peter33032003@gmail.com>
2022-09-21 13:15:02 +0800

    Add revise information on SMBIOS member mapping table.
    Add type 16, 17 o on SMBIOS member mapping table

--- AaeonSmbiosApi/MemberProp.h
+++ AaeonSmbiosApi/MemberProp.h
@@ -6,15 +6,17 @@ public:
 	unsigned char type_;
 	unsigned char offset_;
 	unsigned char length_;
+	bool revise_;
 
 	MemberProp()
 	{
 		type_ = 0xFF;
 		offset_ = 0;
 		length_ = 0;
+		revise_ = false;
 	}
 
-	MemberProp(unsigned char type, unsigned char offset, unsigned char length)
-		: type_(type), offset_(offset), length_(length) {}
+	MemberProp(unsigned char type, unsigned char offset, unsigned char length, bool revise)
+		: type_(type), offset_(offset), length_(length), revise_(revise){}
 };
 
